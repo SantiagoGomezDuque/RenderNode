9d2e888dcb1d90f06d4e84758c32b747176e4c61
SantiagoGomezDuque <santyfomezduque30@gmail.com>
2025-04-22 14:57:16 -0500

    saludo

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
