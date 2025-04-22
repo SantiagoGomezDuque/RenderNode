e61e7866a506742593d685179da4032f4194c36f
SantiagoGomezDuque <santyfomezduque30@gmail.com>
2025-04-22 13:47:54 -0500

    saludo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, don Carmelo!');
+  res.send('¡Buenas tardes caballero ilustre!');
 });
 
 app.listen(port, () => {
